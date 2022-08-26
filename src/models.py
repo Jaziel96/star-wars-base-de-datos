531f0c522fb52f426137f6a89264bae130ba5611
Jaziel96 <jazcov96@hotmail.com>
2022-08-26 02:43:50 +0000

    actualizacion de base de datos star wars

--- src/models.py
+++ src/models.py
@@ -18,36 +18,38 @@ class User(Base):
     lastname = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False, unique=True)
     pasword= Column(String(16), nullable=False, unique=True)
+    fav_personajes = relationship('fav_personajes', backref='user', lazy=True)
+    fav_planetas = relationship('fav_planetas', backref='user', lazy=True)
     
 
-class Planetas(Base):
+class planetas(Base):
     __tablename__ = 'planetas'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id=Column(Integer, primary_key=True)
     Description_text=Column(String(500),nullable=False)
-    pelicula_id=Column(Integer, ForeignKey('Favoritos.id'))
-    pelicula=relationship(Favoritos)
+    planeta_id=Column(Integer, ForeignKey('fav_planetas.id'))
+    fav_planetas = relationship('fav_planetas', backref='user', lazy=True)
 
 class personajes(Base):
     __tablename__="personajes"
     id=Column(Integer, primary_key=True)
     Description_text=Column(String(500),nullable=False)
-    pelicula_id=Column(Integer, ForeignKey('Favoritos.id'))
-    pelicula=relationship(Favoritos)
+    pelicula_id=Column(Integer, ForeignKey('fav_personajes.id'))
+    fav_personajes = relationship('fav_personajes', backref='user', lazy=True)
 
 
-class Favoritos(Base):
-    __tablename__="Favoritos"
-    id=Column(Integer, primary_key=True)
-    user_from_id=Column(Integer, ForeignKey('User.id'))
-    user_from=relationship(User)
-    user_to_id=Column(Integer, ForeignKey('User.id'))
-    user_to=relationship(User)
-    Favoritos_pelicula_id=Column(Integer, ForeignKey('User.id'))
-    Favoritos_pelicula=relationship(User)
-    Favoritos_personajes_id=Column(Integer, ForeignKey('User.id'))
-    Favoritos_personajes=relationship(User)
+class fav_planetas(Base):
+    __tablename__ = 'fav_planetas'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer,ForeignKey('user.id'))
+    planets_id = Column(Integer, ForeignKey('planetas.id'))
+
+class fav_personajes(Base):
+    __tablename__ = 'fav_personajes'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer,ForeignKey('user.id'))
+    personajes_id = Column(Integer, ForeignKey('personajes.id'))
 
     def to_dict(self):
         return {}
